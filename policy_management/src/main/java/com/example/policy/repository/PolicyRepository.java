fca42f24262ed8be392f51751e70effbe8d80681
Brij Bhushan Sharma <156561491+Sharmabrij@users.noreply.github.com>
2025-03-11 17:41:59 +0530

    Delete policy_management directory

--- policy_management/src/main/java/com/example/policy/repository/PolicyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.policy.repository;
-
-import com.example.policy.model.Policy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-}
